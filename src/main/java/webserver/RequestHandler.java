fd8a892ca3bd3e7231efe0898522688516bf2a29
dlwjddus1112 <161396355+dlwjddus1112@users.noreply.github.com>
2024-10-01 15:53:56 +0900

    feat: 요구사항 2 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -16,11 +16,15 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static http.util.HttpRequestUtils.parseQueryParameter;
+
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private final Repository repository;
     public RequestHandler(Socket connection) {
         this.connection = connection;
+        this.repository = MemoryUserRepository.getInstance();
     }
 
     @Override
@@ -34,17 +38,30 @@ public class RequestHandler implements Runnable{
             String[] startLines = startLine.split(" ");
             String method = startLines[0];
             String targetUrl = startLines[1];
-            System.out.println(Arrays.toString(startLines));
-            int requestContentLength = 0;
-
+            System.out.println(targetUrl);
             byte[] body = new byte[0];
 
-            if(method.equals("GET") && targetUrl.contains("index.html")){
-                body = Files.readAllBytes(Paths.get("./webapp" + targetUrl));
+            Path targetPath = Paths.get("./webapp" + targetUrl);
+            if(method.equals("GET") && targetUrl.equals("/index.html")){
+                body = Files.readAllBytes(targetPath);
             }
 
             if(method.equals("GET") && targetUrl.equals("/")){
                 body = Files.readAllBytes(Paths.get("webapp/index.html" ));
+            }
+            if (targetUrl.equals("/user/form.html")) {
+                body = Files.readAllBytes(targetPath);
+            }
+
+            if (method.equals("GET") && targetUrl.contains("/user/signup")) {
+                String queryString = IOUtils.readData(br, body.length);
+                Map<String, String> queryParameter = parseQueryParameter(queryString);
+
+                User user = new User(queryParameter.get("userId"), queryParameter.get("password"), queryParameter.get("name"), queryParameter.get("email"));
+                repository.addUser(user);
+                response302Header(dos,"/index.html");
+
+                return;
             }
 
             response200Header(dos, body.length);
@@ -52,6 +69,17 @@ public class RequestHandler implements Runnable{
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, String path)  {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
         }
     }
 
@@ -74,5 +102,7 @@ public class RequestHandler implements Runnable{
             log.log(Level.SEVERE, e.getMessage());
         }
     }
+
+
 
 }
