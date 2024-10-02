0501a67d54c22045862cbab6754b81c756f3cddf
mr8356 <mr8356@naver.com>
2024-10-02 14:05:33 +0900

    requirement 1 complete

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,13 +2,15 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
-
+    private static final String WEBAPP_DIR = "webapp";
     public RequestHandler(Socket connection) {
         this.connection = connection;
     }
@@ -20,9 +22,34 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            byte[] body;
+            // GET /index.html
+            String request = br.readLine();
+            if (request == null || request.isEmpty()) {
+                return;
+            }
+
+            // 요청 라인 파싱
+            String[] tokens = request.split(" ");
+
+            String method = tokens[0];
+            String path = tokens[1];
+
+            // 요청된 경로에 따라 파일 경로 설정
+            if (path.equals("/")) {
+                path = "/index.html";
+            }
+            String filePath = WEBAPP_DIR + path;
+            File file = new File(filePath);
+            if (file.exists()) {
+                body = Files.readAllBytes(Paths.get(filePath));
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+//            else {
+//                log.log(Level.INFO,"파일 없음!!!!!!");
+//            }
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
